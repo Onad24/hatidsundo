2759c796396963bed68d4980b147932b1e5d0b00
Onad24 <rctmalate@gmail.com>
2026-06-12 06:31:11 +0800

    modified:   build/web/flutter_bootstrap.js 	modified:   build/web/map.html

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"9828a8bf3fe26ba43a8aecf7e8eb1033ac2858
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2916493145" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4068381628" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
